9b69a07b66700b7662254f1a6ac2fb071d0da694
MacielDev <andreraizdetudo@gmail.com>
2022-07-11 20:15:59 -0300

    primeira versão para inserir pacientes de forma dinâmica na planilha de pacientes

--- js/principal.js
+++ js/principal.js
@@ -89,11 +89,60 @@ function exibeResultadoIMC (imc,resultadoImc,paciente){
 
 function alteraCorResultadoIMC (imc,paciente) {
     if(imc.textContent == 'peso inválido!' || imc.textContent == 'altura inválida!'){
-        console.log('erro');
         paciente.classList.remove('imcValido');
         paciente.classList.add('imcInvalido');
     }
 }
+
+/**
+ * INSERINDO NOVOS PACIENTES 
+ */
+
+const formulario = document.querySelector('#form-adiciona');
+formulario.addEventListener('click', (event)=>{
+
+    /**EVITANDO O COMPORTAMENTO PADRÃO DE UM BOTÃO DE SUBMIT
+     *  DE UM FORMULARIO (LIMPAR CAMPOS, ENVIAR DADOS E RECARREGAR PÁGINA*/
+    event.preventDefault();
+
+    /**ACESSANDO OS DADOS DO FORMUÁRIO ATRAVÉS DO ATRIBBUTO 'NAME' DAS TAGS 
+     * 'INPUTS'
+     */
+
+    const nome = formulario.nome.value;
+    const peso = formulario.peso.value;
+    const altura = formulario.altura.value;
+    const gordura = formulario.gordura.value;
+
+    console.log(nome,peso,altura,gordura);
+
+    /** CRIANDO ELENTOS HTML ATRAVÉS DA FUNÇÃO 'CREATE ELEMENT */
+
+     const tabela = document.querySelector('#tabela-pacientes')
+    //  criando <tr>
+     const pacienteTR = document.createElement('tr');
+
+    // Criando as <td> para os dados do novo paciente
+    const nomeTD = document.createElement('td');
+    const pesoTD = document.createElement('td');
+    const alturaTD = document.createElement('td');
+    const gorduraTD = document.createElement('td');
+
+    // Caregando os dados nas <td> 
+    nomeTD.textContent = nome;
+    pesoTD.textContent = peso;
+    alturaTD.textContent = altura;
+    gorduraTD.textContent = gordura;
+
+    pacienteTR.appendChild(nomeTD);
+    pacienteTR.appendChild(pesoTD);
+    pacienteTR.appendChild(alturaTD);
+    pacienteTR.appendChild(gorduraTD);
+
+    tabela.appendChild(pacienteTR);
+
+})
+
 
 
 
