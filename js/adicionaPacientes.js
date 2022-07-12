3101f2dcc2704109f748ba6511a1db1bbc39de7b
MacielDev <andreraizdetudo@gmail.com>
2022-07-11 22:42:53 -0300

    organizando o código

--- js/adicionaPacientes.js
+++ js/adicionaPacientes.js
@@ -1,48 +1,71 @@
+
 /**
  * INSERINDO NOVOS PACIENTES 
  */
 
- const formulario = document.querySelector('#form-adiciona');
- formulario.addEventListener('click', (event)=>{
+ const botaoAcionar = document.querySelector('#adicionar-paciente');
+ botaoAcionar.addEventListener('click', (event)=>{
  
      /**EVITANDO O COMPORTAMENTO PADRÃO DE UM BOTÃO DE SUBMIT
       *  DE UM FORMULARIO (LIMPAR CAMPOS, ENVIAR DADOS E RECARREGAR PÁGINA*/
      event.preventDefault();
  
-     /**ACESSANDO OS DADOS DO FORMUÁRIO ATRAVÉS DO ATRIBBUTO 'NAME' DAS TAGS 
-      * 'INPUTS'
+     const formulario = document.querySelector('#form-adiciona')
+     /**
+      * EXTRAINDO OS DADOS DO PACIENTE DO FORMULÁRIO
       */
- 
-     const nome = formulario.nome.value;
-     const peso = formulario.peso.value;
-     const altura = formulario.altura.value;
-     const gordura = formulario.gordura.value;
- 
-     console.log(nome,peso,altura,gordura);
+    
+     const paciente = obtemInformacoesFormulario(formulario);
+     
+
  
      /** CRIANDO ELENTOS HTML ATRAVÉS DA FUNÇÃO 'CREATE ELEMENT */
+    
+    const pacienteTR = montaTR(paciente);
+      
+     
  
-      const tabela = document.querySelector('#tabela-pacientes')
+     const tabela = document.querySelector('#tabela-pacientes')
      //  criando <tr>
-      const pacienteTR = document.createElement('tr');
- 
-     // Criando as <td> para os dados do novo paciente
-     const nomeTD = document.createElement('td');
-     const pesoTD = document.createElement('td');
-     const alturaTD = document.createElement('td');
-     const gorduraTD = document.createElement('td');
- 
-     // Caregando os dados nas <td> 
-     nomeTD.textContent = nome;
-     pesoTD.textContent = peso;
-     alturaTD.textContent = altura;
-     gorduraTD.textContent = gordura;
- 
-     pacienteTR.appendChild(nomeTD);
-     pacienteTR.appendChild(pesoTD);
-     pacienteTR.appendChild(alturaTD);
-     pacienteTR.appendChild(gorduraTD);
- 
      tabela.appendChild(pacienteTR);
+
+     formulario.reset();
+
+
+ });
+
+
+ function obtemInformacoesFormulario(formulario){
+    const paciente ={
+        nome: formulario.nome.value,
+        peso: Number(formulario.peso.value),
+        altura: Number(formulario.altura.value),
+        gordura: formulario.gordura.value,
+        imc: calculaIMC(formulario.peso.value,formulario.altura.value)
+    }
+    return paciente;
+}
+
+function montaTR(paciente){
+    
+    const pacienteTR = document.createElement('tr');
+    pacienteTR.classList.add('paciente');
+    pacienteTR.classList.add('imcValido');
+   
+    pacienteTR.appendChild(montaTD(paciente.nome,"info-nome"));
+    pacienteTR.appendChild(montaTD(paciente.peso,"info-peso"));
+    pacienteTR.appendChild(montaTD(paciente.altura,"info-altura"));
+    pacienteTR.appendChild(montaTD(paciente.gordura,"info-gordura"));
+    pacienteTR.appendChild(montaTD(paciente.imc,"info-imc"));
+
+    return pacienteTR;
+}
+
+function montaTD(dado,classe){
+    const td = document.createElement('td');
+    td.textContent = dado;
+    td.classList.add(classe);
+    
+    return td;
+}
  
- })
--- js/calculaIMC.js
+++ js/calculaIMC.js
@@ -2,7 +2,6 @@ const pacientes = pegaPacientes();
 pacientes.forEach((paciente)=>{
 
     // ACESSANDO OS DADOS DE CADA PACIENTE
-    const nome = paciente.querySelector('.info-nome');
     const peso = pegaPeso(paciente);
     const altura = pegaAltura(paciente);
     const imcTabela = pegaIMC(paciente);
@@ -15,8 +14,6 @@ pacientes.forEach((paciente)=>{
 
     
 })	
-
-
 
 /*
 calculaIMC : FUNÇÕESÇÃO RESPONSÁVEL POR REALIZAR O CÁLCULO DO IMC DE CADA PACIENTE. RECEBE COMO PARÂMETROS DOIS VALORES DO TIPO NUMBER (PESO E ALTURA). SEU RETORNO É O IMC CALCULADO COM UMA APROXIMAÇÃO DE DUAS CASAS DECIMAIS.
@@ -36,8 +33,6 @@ const pesoValido = validaPeso(peso);
          return 'altura inválida!';
      }
  }
-
-
 
 
 
