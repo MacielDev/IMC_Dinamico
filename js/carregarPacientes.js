1e066fa5e88177ceba8c9de92a2c4daf7ab34a04
MacielDev <andreraizdetudo@gmail.com>
2022-07-17 19:14:29 -0300

    carregando pacientes através de api , utilizando a biblioteca Fetch

--- js/carregarPacientes.js
+++ js/carregarPacientes.js
@@ -2,25 +2,19 @@ const botaoCarregarPacientes  = document.querySelector('#carregar-pacientes');
 botaoCarregarPacientes.addEventListener('click', (event)=>{
     event.preventDefault();
     console.log('devo efetuar a carga da api');
-
     const formulario = document.querySelector('#form-adiciona')
-
     fetch('http://api-pacientes.herokuapp.com/pacientes')
     .then((response)=> {
         response.json()
         .then((data)=>{
-
             data.forEach(paciente => {
                 adicionaPaciente(formulario,paciente);
             });
         });
     })
-    
-   
     .catch((err)=>{
         console.error(err);
-    })
-   
+    })  
 });
 
 
