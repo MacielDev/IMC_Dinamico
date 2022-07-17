7f0b401e501c5db53c7a6434c73d72559d5e4214
MacielDev <andreraizdetudo@gmail.com>
2022-07-17 19:10:24 -0300

    carregando pacientes através de api , utilizando a biblioteca Fetch

---
+++ js/filtra.js
@@ -0,0 +1,25 @@
+const campoDeBusca = document.querySelector('#filtrar-tabela');
+campoDeBusca.addEventListener('input',(event)=>{
+    let nomeBuscado = event.target.value;
+   
+    var pacientes = document.querySelectorAll('.paciente');
+    
+    if(nomeBuscado.length > 0){
+        pacientes.forEach((paciente)=> {
+            const tdNome = paciente.querySelector('.info-nome');
+            const nome = tdNome.textContent;
+            const expressaoBusca = new RegExp(nomeBuscado, "i");
+            if(!expressaoBusca.test(nome)){
+                paciente.classList.add('escondePaciente');
+            }else{
+                paciente.classList.remove('escondePaciente');
+            }
+        });
+    }else{
+        pacientes.forEach((paciente)=>{
+            paciente.classList.remove('escondePaciente');
+        });
+    }
+       
+   
+});
